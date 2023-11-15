57b2221f4c8dc8ce7403b5a067d0168560d9d1d8
Bayu Hadi Leksana <bayuhadi863@gmail.com>
2023-11-15 19:06:55 +0700

    menambahkan club_players dan membuat insert matches

---
+++ DML/insert_matches.sql
@@ -0,0 +1,4 @@
+INSERT INTO matches (name, club_home_id, club_away_id, stadium_id, 
+					 match_time, home_score, away_score, referee_id, man_of_the_match)
+VALUES
+('Premier League Match Day 25', 9, 14, 1, '2023-01-14 21:00:00', 2, 1, 1, 30);
